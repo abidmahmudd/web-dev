1bb358f6877eaf9a740f3db7f9a17ea879d8c7ad
abidmahmudd <116264385+abidmahmudd@users.noreply.github.com>
2022-11-15 17:20:58 +0600

    Add files via upload

---
+++ abidday12/abid.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="one">
+    <!-- Abid MAHMUD
+<h1>Dhaka Polytecnic Instiute</h1> -->
+
+<?php
+// echo "<h1>Computer<h1/>"
+    $num1=40;
+    $num2=56;
+    $result=$num1+$num2;
+    echo "Result = ".$result;
+
+?>
+    </div>
+   <div class="two">
+    <?php
+        echo "<h5>Menu<h5/>"
+    ?>
+   </div>
+</body>
+</html>
